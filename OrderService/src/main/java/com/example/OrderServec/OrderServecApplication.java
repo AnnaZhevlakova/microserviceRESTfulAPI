c1d661b3077cada348566066d2c0233e69b88c58
Anna Zhevlakova <azevlakova12@gmail.com>
2025-10-31 20:26:37 +0400

    Order Service

---
+++ OrderService/src/main/java/com/example/OrderServec/OrderServecApplication.java
@@ -0,0 +1,13 @@
+package com.example.OrderServec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServecApplication.class, args);
+	}
+
+}
---
+++ OrderService/src/test/java/com/example/OrderServec/OrderServecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OrderServec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
